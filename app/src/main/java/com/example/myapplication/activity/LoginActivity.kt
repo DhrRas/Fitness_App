22f363dca1737c89ae22708dd1746b3b15cb9ad0
Dhruv <rastogi.dhruv@gmail.com>
2023-10-24 21:57:23 +0530

    a4

--- app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/LoginActivity.kt
@@ -2,11 +2,37 @@ package com.example.myapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityLoginBinding
+import com.example.myapplication.global.DB
+import com.example.myapplication.manager.SessionManager
 
 class LoginActivity : AppCompatActivity() {
+    var db: DB?=null
+    var session:SessionManager?=null
+    var edtUserName: EditText?=null
+    var edtPassword: EditText?=null
+
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = DB(this)
+        session = SessionManager(this)
+        edtUserName = binding.edtUserName
+        edtPassword = binding.edtPassword
+
+        binding.btnLogin.setOnClickListener {
+
+        }
+
+        binding.txtForgotPassword.setOnClickListener {
+
+        }
     }
 }
+
+
