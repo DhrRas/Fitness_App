ca3740cfce472ae63686567b447b36d693f22bb0
Dhruv <rastogi.dhruv@gmail.com>
2023-10-25 00:42:01 +0530

    a5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 24
-        //noinspection OldTargetApi
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -43,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20-RC")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -51,10 +51,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:kotlin_version")
-    implementation("com.github.florent37:runtime-permission:1.1.2")
+    implementation("com.github.florent37:runtime-permission-kotlin:1.2.3")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.amitshekhar.android:debug-db:1.0.6")
+    debugImplementation("com.amitshekhar.android:debug-dg-encrypt:1.0.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
