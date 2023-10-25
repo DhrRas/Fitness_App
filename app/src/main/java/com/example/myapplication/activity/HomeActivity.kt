eb9b41d68371ed828f5135264a2b8a6bbad59142
Dhruv <rastogi.dhruv@gmail.com>
2023-10-25 18:56:08 +0530

    a9

--- app/src/main/java/com/example/myapplication/activity/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/activity/HomeActivity.kt
@@ -1,14 +1,134 @@
 package com.example.myapplication.activity
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.window.OnBackInvokedDispatcher
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityHomeBinding
+import com.example.myapplication.global.DB
+import com.example.myapplication.manager.SessionManager
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
+    private val TAG = "HomeActivity"
+    var session: SessionManager? = null
+    var db: DB? = null
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
     lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        db = DB(this)
+        session = SessionManager(this)
+        setSupportActionBar(binding.homeInclude.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.navView.setNavigationItemSelectedListener(this)
+        drawer = binding.drawerLayout
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            binding.homeInclude.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+            R.id.nav_add -> {
+                Toast.makeText(this, "Add", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+            R.id.nav_nav_fee_pending -> {
+                Toast.makeText(this, "Pending", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+            R.id.nav_update_fee -> {
+                Toast.makeText(this, "update fee", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+            R.id.nav_change_password -> {
+                Toast.makeText(this, "change password", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+            R.id.nav_log_out -> {
+                Toast.makeText(this, "log out", Toast.LENGTH_LONG).show()
+
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START)
+                }
+            }
+
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
+
+
+
